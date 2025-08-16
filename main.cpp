a22d83b14f20fa00cf3d20713ec6cad903c66ad6
starstalker9160 <abyazjschool@gmail.com>
2025-08-16 21:46:39 +0400

    that was a lie, now im done with mac build

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 #include <QFile>
 #include <QLabel>
 #include <QTimer>
-#include <QDebug>
+#include <QColor>
 #include <QWidget>
+#include <QPalette>
 #include <QDateTime>
 #include <QKeyEvent>
 #include <QJsonObject>
@@ -18,19 +19,23 @@ struct ClockConfig {
     QString fontFile   = "font.otf";
 };
 
+QString getResourcePath(const QString &filename) {
+#ifdef Q_OS_MAC
+    QDir dir(QCoreApplication::applicationDirPath());
+    dir.cdUp();
+    dir.cd("Resources");
+    return dir.filePath(filename);
+#else
+    return QCoreApplication::applicationDirPath() + "/" + filename;
+#endif
+}
+
 ClockConfig loadConfig() {
     ClockConfig config;
-    QString exeDir = QCoreApplication::applicationDirPath();
-
-    QString suffix = "clock.app/Contents/MacOS";
-    if (exeDir.endsWith(suffix)) { exeDir = exeDir.chopped(suffix.length()); }
-
-    QFile file(exeDir + "/config.json");
-
+    QFile file(getResourcePath("config.json"));
     if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
         QByteArray data = file.readAll();
         file.close();
-
         QJsonDocument doc = QJsonDocument::fromJson(data);
         if (doc.isObject()) {
             QJsonObject obj = doc.object();
@@ -40,71 +45,50 @@ ClockConfig loadConfig() {
                 config.background = obj["background"].toString();
             if (obj.contains("font") && obj["font"].isString())
                 config.fontFile = obj["font"].toString();
-        } else {
-            qWarning() << "config.json is not a valid JSON object, using defaults.";
         }
-    } else {
-        qWarning() << "No config.json found, using defaults.";
     }
-
     return config;
 }
 
 class Clock : public QWidget {
     Q_OBJECT
-
 public:
     Clock(const ClockConfig &config) : cfg(config) {
-        QString exeDir = QCoreApplication::applicationDirPath();
-        QString suffix = "clock.app/Contents/MacOS";
-        if (exeDir.endsWith(suffix)) { exeDir = exeDir.chopped(suffix.length()); }
-
-        QString fontPath = exeDir + "/" + cfg.fontFile;
-
+        QString fontPath = getResourcePath(cfg.fontFile);
         int id = QFontDatabase::addApplicationFont(fontPath);
         QString family;
         if (id != -1) {
             family = QFontDatabase::applicationFontFamilies(id).value(0);
         }
-
         QFont font;
         if (!family.isEmpty()) {
             font = QFont(family, 72);
         } else {
-            qWarning() << "Font" << cfg.fontFile << "could not be loaded, falling back to default.";
             font = QFont("Sans Serif", 72);
         }
-
         timeLabel = new QLabel(this);
         timeLabel->setAlignment(Qt::AlignCenter);
         timeLabel->setFont(font);
         timeLabel->setStyleSheet(QString("QLabel { color: %1; background-color: transparent; }")
                                      .arg(cfg.foreground));
-
         QVBoxLayout *layout = new QVBoxLayout(this);
         layout->addWidget(timeLabel);
         setLayout(layout);
-
         setWindowTitle("Clock");
-
         setAutoFillBackground(true);
         QPalette pal = palette();
         pal.setColor(QPalette::Window, QColor(cfg.background));
         setPalette(pal);
-
         currentTime = QDateTime::currentDateTime();
         updateLabel();
-
         timer = new QTimer(this);
         connect(timer, &QTimer::timeout, this, &Clock::tick);
-
         int msToNextSecond = 1000 - QTime::currentTime().msec();
         QTimer::singleShot(msToNextSecond, this, [this]() {
             timer->start(1000);
             tick();
         });
     }
-
 protected:
     void keyPressEvent(QKeyEvent *event) override {
         if (event->key() == Qt::Key_Return && (event->modifiers() & Qt::AltModifier)) {
@@ -114,18 +98,15 @@ protected:
                 showFullScreen();
         }
     }
-
 private slots:
     void tick() {
         currentTime = currentTime.addSecs(1);
         updateLabel();
     }
-
 private:
     void updateLabel() {
         timeLabel->setText(currentTime.toString("HH:mm:ss"));
     }
-
     QLabel *timeLabel;
     QTimer *timer;
     QDateTime currentTime;
@@ -134,11 +115,9 @@ private:
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-
     ClockConfig cfg = loadConfig();
     Clock clock(cfg);
     clock.show();
-
     return app.exec();
 }
 
