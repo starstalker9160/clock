891c7c9f66018e8590b1011855251a79371bd032
star.stalker9160 <abyazjschool@gmail.com>
2025-08-16 08:27:43 +0400

    added config json support

--- main.cpp
+++ main.cpp
@@ -1,28 +1,75 @@
+#include <QDir>
+#include <QFile>
 #include <QLabel>
 #include <QTimer>
+#include <QDebug>
 #include <QWidget>
-#include <QKeyEvent>
 #include <QDateTime>
+#include <QKeyEvent>
+#include <QJsonObject>
 #include <QVBoxLayout>
 #include <QApplication>
+#include <QJsonDocument>
 #include <QFontDatabase>
 
-const char* BACKGROUND_COLOR = "#000000";
-const char* FOREGROUND_COLOR = "#00FF00";
+struct ClockConfig {
+    QString foreground = "#00ff00";
+    QString background = "#000000";
+    QString fontFile   = "font.otf";
+};
 
-class Clock : public QWidget {
-    Q_OBJECT
+ClockConfig loadConfig() {
+        ClockConfig config;
+        QString exeDir = QCoreApplication::applicationDirPath();
+        QFile file(exeDir + "/config.json");
+
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            QByteArray data = file.readAll();
+            file.close();
+
+            QJsonDocument doc = QJsonDocument::fromJson(data);
+            if (doc.isObject()) {
+                QJsonObject obj = doc.object();
+                if (obj.contains("foreground") && obj["foreground"].isString())
+                    config.foreground = obj["foreground"].toString();
+                if (obj.contains("background") && obj["background"].isString())
+                    config.background = obj["background"].toString();
+                if (obj.contains("font") && obj["font"].isString())
+                    config.fontFile = obj["font"].toString();
+            } else {
+                qWarning() << "config.json is not a valid JSON object, using defaults.";
+            }
+        } else {
+            qWarning() << "No config.json found, using defaults.";
+        }
+
+        return config;
+    }
+
+    class Clock : public QWidget {
+        Q_OBJECT
 
     public:
-        Clock() {
-            int id = QFontDatabase::addApplicationFont("font.otf");
-            QString family = QFontDatabase::applicationFontFamilies(id).value(0);
-            QFont font(family, 72, QFont::Bold);
+        Clock(const ClockConfig &config) : cfg(config) {
+            QString fontPath = QCoreApplication::applicationDirPath() + "/" + cfg.fontFile;
+            int id = QFontDatabase::addApplicationFont(fontPath);
+            QString family;
+            if (id != -1) {
+                family = QFontDatabase::applicationFontFamilies(id).value(0);
+            }
+
+            QFont font;
+            if (!family.isEmpty()) {
+                font = QFont(family, 72, QFont::Bold);
+            } else {
+                qWarning() << "Font" << cfg.fontFile << "could not be loaded, falling back to default.";
+                font = QFont("Sans Serif", 72, QFont::Bold);
+            }
 
             timeLabel = new QLabel(this);
             timeLabel->setAlignment(Qt::AlignCenter);
             timeLabel->setFont(font);
-            timeLabel->setStyleSheet(QString("QLabel { color: %1; background-color: transparent; }").arg(FOREGROUND_COLOR));
+            timeLabel->setStyleSheet(QString("QLabel { color: %1; background-color: transparent; }").arg(cfg.foreground));
 
             QVBoxLayout *layout = new QVBoxLayout(this);
             layout->addWidget(timeLabel);
@@ -32,7 +79,7 @@ class Clock : public QWidget {
 
             setAutoFillBackground(true);
             QPalette pal = palette();
-            pal.setColor(QPalette::Window, QColor(BACKGROUND_COLOR));
+            pal.setColor(QPalette::Window, QColor(cfg.background));
             setPalette(pal);
 
             currentTime = QDateTime::currentDateTime();
@@ -72,12 +119,16 @@ class Clock : public QWidget {
         QLabel *timeLabel;
         QTimer *timer;
         QDateTime currentTime;
+        ClockConfig cfg;
 };
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    Clock clock;
+
+    ClockConfig cfg = loadConfig();
+    Clock clock(cfg);
     clock.show();
+
     return app.exec();
 }
 
